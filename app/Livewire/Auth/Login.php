62e3e8ccb4061b4d46e3e4c622a8b9ef17240352
stephen042 <makuochukwu042@gmail.com>
2025-09-10 03:50:25 +0100

    login update

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -42,10 +42,10 @@ class Login extends Component
             session()->regenerate();
 
             // Redirect by role
-            if ($user->role != 1) {
-                redirect()->route('app.dashboard');
-            } else {
+            if ($user->role == 1) {
                 redirect()->route('admin.dashboard');
+            } else {
+                redirect()->route('app.dashboard');
             }
 
             // Send login alert email
